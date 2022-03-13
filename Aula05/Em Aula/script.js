f1c2f6a2d6a2ec7c50421d39cb13a87e169d600f
Williannca85 <87399058+Williannca85@users.noreply.github.com>
2022-03-13 17:06:14 -0300

    Adicionado aula 05

---
+++ Aula05/Em Aula/script.js
@@ -0,0 +1,17 @@
+/* let titleReference = document.querySelector('#title');
+
+titleReference.classList.add('title'); */
+
+let mainMenuReference = document.querySelector('#mainMenu')
+
+function openMenu(){
+
+     mainMenuReference.classList.add('main-menu-opened')
+
+}
+
+function closeMenu(){
+
+     mainMenuReference.classList.remove('main-menu-opened')
+
+}
